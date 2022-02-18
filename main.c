0bb1082937d24b19936fe12e13e077fcc639db9e
John Lins <johnpaullins@gmail.com>
2022-02-18 15:39:59 -0800

    extend lim def

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <math.h>
-#include "raylib.h"
+#include <stdlib.h> 
+//#include "raylib.h"
 
 //Hackathon: https://ypstemhackathon2022.devpost.com/?ref_feature=challenge&ref_medium=discover
 
 #define SW 1000
 #define SH 800
 #define STEP 10
-
+/*
 typedef struct Dual {
   double real;
   double dual;
@@ -40,21 +41,128 @@ double first_derivative(Dual (*f)(Dual), double x){
     return (*f)((Dual){x,1}).dual;
 }
 
-double higher_derivative(Dual (*f)(Dual), double x, int n) {
+
+
+Dual dd(Dual (*f)(Dual), double x){
+  Dual v = (*f)((Dual){x,1});
+  return (Dual){ v,real, v.dual};
+}
+*/
+
+/*double higher_derivative(Dual (*f)(Dual), double x, int n) {
     
     double h = .00001;
     
    return  ((*f)((Dual){x + h,1}).real - n*((*f)((Dual){x,1}).real) + (*f)((Dual){x-h,1}).real)/powf(h,n);
     
     
+}*/
+
+
+void fib(int *fibs, int len){
+    
+    //init
+    int hold[len+1];
+    for(int i = 0; i < len; i++){fibs[i] = 1; hold[i] = 1;} hold[len] = 1;
+    
+
+  if (len % 2 == 0) { //even
+    for(int i = 0; i < len - 2; i++){
+        
+        if(i % 2 == 0){
+            
+            for(int j = 0; j <= i/2; j++){
+                
+                hold[len/2 + j] = fibs[len/2 - 1 + j] + fibs[len/2 + j];
+                hold[len/2 - j] = fibs[len/2 - 1 + j] + fibs[len/2 + j];
+                
+            }
+            ////printf
+              //  printf("HOLD(%d): ",i);for(int k = 0; k < len+1; k++){ printf("%d ", hold[k]);} printf("\n");
+                ////
+        } else {
+        
+            for(int j = 0; j <= i/2; j++){
+                fibs[len/2 - 1 - j] = hold[len/2 + j] + hold[len/2 + 1 + j];
+                fibs[len/2 + j] = hold[len/2 + j] + hold[len/2 + 1 + j];
+                
+            }
+           // printf("FIB (%d): ",i);PP(fibs, len);
+        }
+        
+       
+        
+    }
+    
+
+  } else {//odd
+  
+    
+
+      for(int i = 0; i < len - 2; i++){
+        
+        if(i % 2 == 0){
+            
+            for(int j = 0; j <= i/2; j++){
+                
+                fibs[len/2 + j] = hold[len/2 - 1 + j] + hold[len/2 + j];
+                fibs[len/2 - j] = hold[len/2 - 1 + j] + hold[len/2 + j];
+                
+            }
+            ////printf
+              //  printf("HOLD(%d): ",i);for(int k = 0; k < len+1; k++){ printf("%d ", hold[k]);} printf("\n");
+                ////
+        } else {
+        
+            for(int j = 0; j <= i/2; j++){
+                hold[len/2 - 1 - j] = fibs[len/2 + j] + fibs[len/2 + 1 + j];
+                hold[len/2 + j] = fibs[len/2 + j] + fibs[len/2 + 1 + j];
+                
+            }
+           // printf("FIB (%d): ",i);PP(fibs, len);
+        }
+        
+       
+        
+    }
+ 
+  }
+  
 }
 
-//pass function pointer
-double taylor(double a, double x, int iteration, Dual (*f)(Dual)){
+/*
+double first(double (*f)(double), double x){
+  double h = .000001;
+
+  return   ((*f)(x+h) - (*f)(x))/h;
+
+}
+*/
+
+double higher(double (*f)(double), double x, double n){
+  double h = .000001;
+
+  int *fibs = (int*)malloc((n + 1) * sizeof(int));
+  fib(fibs, n+1);
+
+  double numer = 0;
+
+  for(int i = 0; i <= n; i++){
+    numer += pow(-1, i) * fibs[i] * (*f)(x + (n-i)*h);
+
+  }
+
+  free(&fibs);
+
+  return numer / (pow(h, n));
+}
+
+
+double taylor(double a, double x, int iteration, double (*f)(double)){
     double y = 0;
     long long int unsigned  factorial = 1;
     for(int i = 1; i < iteration; i++){
-        double derivative = higher_derivative(f,a,i);//(*f)((Dual){a,1}).dual;
+        double derivative = higher(f,a,i);//(*f)((Dual){a,1}).dual;
         //^You are not taking higher order derivates!!
         y += (derivative / factorial) * powf(x - a, i-1);
         
@@ -65,12 +173,25 @@ double taylor(double a, double x, int iteration, Dual (*f)(Dual)){
     return y;
 }
 
+double func(double x){
+  return 30 * sinf(.02*x);
+}
+
+double test(double x){
+    return cosf(x);
+}
+
+
 int main()
 {
  
 
-
-
+printf("df = %f\n", higher(&test, 0, 0));
+printf("df = %f\n", higher(&test, 0, 1));
+printf("df = %f\n", higher(&test, 0, 2));
+printf("df = %f\n", higher(&test, 0, 3));
+printf("df = %f\n", higher(&test, 0, 4));
+/*
     InitWindow(SW, SH, "Taylor Series Sim | John Lins");
 
     double num_of_it = 1;
@@ -87,8 +208,8 @@ int main()
             DrawLine(0, SH/2, SW, SH/2, BLACK);
 
             for(int i = -SW/2; i < SW/2; i++){
-                DrawCircle(i + SW/2, fd((Dual){i,1}).real + SH/2, 2, BLUE);
-                DrawCircle(i + SW/2, taylor(0, i, (int)num_of_it, &fd) + SH/2, 2, RED);
+                DrawCircle(i + SW/2, func(i)+ SH/2, 2, BLUE);
+                DrawCircle(i + SW/2, taylor(0, i, (int)num_of_it, &func) + SH/2, 2, RED);
                 //printf("::: %f", taylor(0, i, (int)num_of_it));
             }//fd((Dual){i/SW * STEP, 1}).real
             
@@ -101,7 +222,7 @@ int main()
     }
 
  
-    CloseWindow();     
+    CloseWindow();   */
 
   return 0;
 }
